3b5a48c0f739d792a1f9ff7c5fc33d10d231d827
Salaarop07 <129603763+Salaarop07@users.noreply.github.com>
2023-04-26 20:18:36 +0530

    Update config.py

--- NibiMusic/config.py
+++ NibiMusic/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID", "13831288"))
-API_HASH = getenv("API_HASH", "0d4c67b8d1bef020475434abc394ac4c")
+API_ID = int(getenv("API_ID", "22591542"))
+API_HASH = getenv("API_HASH", "66e5f190dd81d951674d8edc78f162e5)
 BOT_USERNAME = getenv("BOT_USERNAME")
 BOT_TOKEN = getenv("BOT_TOKEN")
 UPDATE_CHANNEL = getenv("UPDATE_CHANNEL")
@@ -16,5 +16,5 @@ SUPPORT_GROUP = getenv("SUPPORT_GROUP")
 OWNER_USERNAME = getenv("OWNER_USERNAME")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 STRING_SESSION = getenv("STRING_SESSION")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6190680150").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5715680680").split()))
 aiohttpsession = aiohttp.ClientSession()
